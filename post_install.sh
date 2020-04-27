5c16f62ffe46a89c54ea773afb49bee1308db7ad
yudjinn <jacob.rodgers94@gmail.com>
2020-04-27 00:19:12 -0600

    added surface stuff

--- post_install.sh
+++ post_install.sh
@@ -4,10 +4,23 @@
 #Add repos
 sudo add-apt-repository -y ppa:alessandro-strada/ppa
 
+
+#For surface only
+wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
+    | sudo apt-key add -
+echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" | sudo tee /etc/apt/sources.list.d/linux-surface.list
+
+
+
 #install packages
 sudo apt-get update
 sudo apt-get install -y google-drive-ocamlfuse git
 snap install -y atom krita
+
+#Surface only installation
+sudo apt-get install linux-headers-surface linux-image-surface linux-libc-dev-surface surface-ipts-firmware libwacom-surface
+sudo apt-get install linux-surface-secureboot-mok
+
 
 #git setup
 git config --global user.name yudjinn
