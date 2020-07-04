6f754d7e58669c728ffda5336f2381813e892b10
Jacob Rodgers <yudjinncoding@gmail.com>
2020-07-04 12:14:39 -0600

    updated

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Post-installation script
 
-echo "Run this script as sudo"
 
 #User input
 echo "Is this a laptop?Y|N"
@@ -14,87 +13,88 @@ read googstate
 
 #Add repos
 echo "Adding Repos..."
-add-apt-repository -y ppa:alessandro-strada/ppa universe
+sudo add-apt-repository universe
 
 #For surface only
-if [[$surfstate =~ ^[Yy]$ ]]
+if [[ $surfstate =~ ^[Yy]$ ]]
 then
 echo "Adding surfacelinux repos..."
-wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
-    | sudo apt-key add - &
+sudo wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
+    | sudo apt-key add - 
 echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" | sudo tee /etc/apt/sources.list.d/linux-surface.list &
 fi
 
 #snaps and debs
-snaps = (spotify android-messages-desktop code krita chromium unofficial-webapp-office)
-debs = (gnome-tweak-tool python3 git terminology apt-transport-https ca-certificates curl gnupg-agent software-properties common)
+snaps=(spotify android-messages-desktop code krita chromium unofficial-webapp-office)
+debs=(gnome-tweak-tool python3 git terminology apt-transport-https ca-certificates curl gnupg-agent software-properties-common)
 
 #install packages
 echo "Installing base packages"
-apt-get update &
-BACK_PID = $!
+sudo apt-get update 
+BACK_PID=$!
 wait $BACK_PID
-if [[$googstate =~ ^[Yy]$]]
+if [[ $googstate =~ ^[Yy]$ ]]
 then
-apt-get install -y google-drive-ocamlfuse git
+sudo apt install -y google-drive-ocamlfuse git
 fi
 echo "Debs first:"
 echo $debs
-apt-get install -y ${debs} &
-$BACK_PID = $!
+sudo apt install -y ${debs[*]} 
+BACK_PID=$!
 wait $BACK_PID
 echo "Snaps now:"
 echo $snaps
-snap install -y ${snaps} &
-$BACK_PID = $!
+sudo snap install -y ${snaps[*]} 
+BACK_PID=$!
 wait $BACK_PID
 echo "Removing that pesky firefox..."
-apt-get remove firefox
+sudo apt remove firefox
 
 #Install docker
 echo "Installing Docker..."
-curl -fsSL https://download.docker.com/linux/debian/gpd | apt-key add - 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 
-apt-get update &
-$BACK_PID = $!
-wait $BACK_PID
-apt-get install docker-ce docker-ce-cli containerd.io 
-usermod -aG docker $USER 
+sudo apt install -y docker.io
+#curl -fsSL https://download.docker.com/linux/debian/gpd | apt-key add - 
+#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 
+#apt-get update 
+#$BACK_PID=$!
+#wait $BACK_PID
+#apt install docker-ce docker-ce-cli containerd.io 
+sudo usermod -aG docker $USER 
 
 #Surface only installation
-if [[$surfstat =~ ^[Yy]$ ]]
+if [[ $surfstate =~ ^[Yy]$ ]]
 then
 echo "Surfacelinux package installation..."
-apt-get install -y linux-headers-surface linux-image-surface linux-libc-dev-surface surface-ipts-firmware libwacom-surface
-apt-get install -y linux-surface-secureboot-mok
+sudo apt install -y linux-headers-surface linux-image-surface linux-libc-dev-surface surface-ipts-firmware libwacom-surface
+sudo apt install -y linux-surface-secureboot-mok
 fi
 
 #Laptop only installation
-if [[$lapstate =~ ^[Yy]$ ]]
+if [[ $lapstate =~ ^[Yy]$ ]]
 then
 echo "Installing laptop performance packages"
-apt-get install -y tlp powertop
-tlp start 
+sudo apt install -y tlp powertop
+sudo tlp start 
 fi
 
 #git setup
-echo "Setting git config..."
-git config --global user.name yudjinn
-git config --global user.email yudjinncoding@gmail.com
+#echo "Setting git config..."
+#git config --global user.name "yudjinn"
+#git config --global user.email "yudjinncoding@gmail.com"
 
 #start google drive util for authorization in webpage
-if [[$googstate =~ ^[Yy]$ ]]
+if [[ $googstate =~ ^[Yy]$ ]]
 then
 echo "Starting google drive mount util..."
 google-drive-ocamlfuse
 #make directory and mount google drive to it
-mkdir ~/google-drive
-google-drive-ocamlfuse ~/google-drive
+mkdir -p ~/google-drive
+sudo google-drive-ocamlfuse ~/google-drive
 fi
 
 #Configure bashrc and kakrc
 echo "Configuring bashrc and kakrc.."
 echo -e "\n\nexport XDG_CONFIG_HOME=\$HOME/.config">>$HOME/.bashrc
 source $HOME/.bashrc
-mkdir $HOME/.config/kak
+mkdir -p $HOME/.config/kak
 echo "hook global WinCreate ^[^*]+$ %{ add-highlighter window/ number-lines -hlcursor }">$HOME/.config/kak/kakrc
